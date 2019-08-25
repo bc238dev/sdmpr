e6877f2325239aa06e170975cd9272c2317b9052
bc238dev <bc238dev@gmail.com>
2019-08-25 00:41:05 -0700

    Added another test.

--- test/sdmpr.test.ts
+++ test/sdmpr.test.ts
@@ -14,4 +14,19 @@ describe('SimpleDataMapper', () => {
     expect(data2.world).exist
     expect(data2.world).to.equal("Hello there")
   })
+
+  it("Should map multiple fields", () => {
+    const mapper1 = SimpleDataMapper.create()
+
+    mapper1.map("first_name", "firstName")
+    mapper1.map("last_name", "lastName")
+    mapper1.map("age")
+
+    const data1 = { first_name: "Evo", last_name: "Zumo", age: 16 }
+    const data2 = mapper1.transform(data1)
+
+    expect(data2.firstName).to.equal("Evo")
+    expect(data2.lastName).to.equal("Zumo")
+    expect(data2.age).to.equal(16)
+  })
 })
