9a981a6966c584b397a08eff555fe78e9ffba9ab
bc238dev <bc238dev@gmail.com>
2019-08-25 01:06:32 -0700

    Moved examples to src

--- src/examples/example1.ts
+++ src/examples/example1.ts
@@ -1,9 +1,9 @@
-import { SimpleDataMapper } from "../src/SimpleDataMapper"
+import { SimpleDataMapper } from "../SimpleDataMapper"
 
 const mapper1 = SimpleDataMapper.create()
   .map("first_name", "firstName")
   .map("last_name", "lastName")
-  .map("age")
+  .map("ages")
 
 const data1 = { first_name: "Evo", last_name: "Zumo", age: 16 }
 const data2 = mapper1.transform(data1)
@@ -12,5 +12,5 @@ const log = (msg: string, obj: any) => {
   console.log(msg, JSON.stringify(obj, null, 2))
 }
 
-log("Data1", data1)
-log("Data2", data2)
+log("Data1->", data1)
+log("Data2->", data2)
