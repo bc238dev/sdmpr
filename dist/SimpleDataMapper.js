53af12779281f73fc8f07d904e537fb7eee0856b
omt66 <omt66@yahoo.com>
2019-08-24 22:53:53 -0700

    Initial commit

---
+++ dist/SimpleDataMapper.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Simple Data Mapper
+ */
+class SimpleDataMapper {
+    constructor(report = false) {
+    }
+}
+exports.SimpleDataMapper = SimpleDataMapper;
---
+++ src/SimpleDataMapper.ts
@@ -0,0 +1,9 @@
+/**
+ * Simple Data Mapper
+ */
+export class SimpleDataMapper {
+  constructor(report = false) {
+
+  }
+
+}
